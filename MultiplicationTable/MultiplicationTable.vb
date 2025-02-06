1d12b630c361129e4d06d4b4127af6fed305c182
Payden Hoskins <paydenhoskins@gmail.com>
2025-02-06 08:02:53 -0700

    Add project files.

---
+++ MultiplicationTable/MultiplicationTable.vb
@@ -0,0 +1,15 @@
+﻿'Payden Hoskins
+'RCET2265
+'Spring 2025
+'Say My Name... Again
+
+Option Explicit On
+Option Strict On
+
+Module MultiplicationTable
+
+    Sub Main()
+
+    End Sub
+
+End Module
